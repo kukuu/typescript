a6033b81823db26838b93272e9c972a757c60f6d
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-07-19 10:42:40 +0100

    Extended interface with age property

--- class.js
+++ class.js
@@ -1,14 +1,15 @@
 var Student = (function () {
-    function Student(firstName, middleInitial, lastName) {
+    function Student(firstName, middleInitial, lastName, age) {
         this.firstName = firstName;
         this.middleInitial = middleInitial;
         this.lastName = lastName;
-        this.fullName = firstName + " " + middleInitial + " " + lastName;
+        this.age = age;
+        this.fullName = firstName + " " + middleInitial + " " + lastName + age;
     }
     return Student;
 }());
 function greeter(person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
+    return "Hello, " + person.firstName + " " + person.lastName + " , age" + person.age;
 }
-var user = new Student("Jane", "M.", "User");
+var user = new Student("Jane", "M.", "Dean", 10);
 document.body.innerHTML = greeter(user);
--- class.ts
+++ class.ts
@@ -1,19 +1,22 @@
 class Student {
     fullName: string;
-    constructor(public firstName, public middleInitial, public lastName) {
-        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    constructor(public firstName, public middleInitial, public lastName, public age) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName + age;
     }
 }
 
+//Creates a type and simplifies the architectural complexity in the returning function(method).
 interface Person {
     firstName: string;
     lastName: string;
+    age: number;
 }
 
 function greeter(person : Person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
+    return "Hello, " + person.firstName + " " + person.lastName + " , age"  + person.age;
 }
 
-var user = new Student("Jane", "M.", "User");
+//Instantiate an object from te class
+var user = new Student("Jane", "M.", "Dean", 10);
 
 document.body.innerHTML = greeter(user);
